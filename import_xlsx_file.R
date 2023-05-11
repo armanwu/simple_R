f1f3faa8fccd0980908ef4f964f739b30b2cd6ca
Arman Arisman <arman.wu@gmail.com>
2023-05-11 10:38:44 +0700

    Update max and min

--- import_xlsx_file.R
+++ import_xlsx_file.R
@@ -1,11 +1,22 @@
 # import library
 library(readxl)
 
+# read Sheet1 from sample.xlsx file 
+read_excel('sample.xlsx', 'Sheet1')
+
 # create variable
-data_from_excel = read_excel('sample.xlsx')
+data_excel = read_excel('sample.xlsx', 'Sheet1')
 
 # show data
-data_from_excel
+data_excel
 
 # view data frame
 View(data_from_excel)
+
+# show maximum and minimum from all cells
+max(data_excel)
+min(data_excel)
+
+# show maximum and minimum from Data1 column
+max(data_excel$Data1)
+min(data_excel$Data1)
